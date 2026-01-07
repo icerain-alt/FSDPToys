f1543afc16acbbbc74f46ff786e3f2a2fe4ffe40
icerain-alt <450125138@qq.com>
2026-01-07 10:26:39 +0800

    update training script

--- run_fsdp.sh
+++ run_fsdp.sh
@@ -1,3 +1,8 @@
-mkdir -p .logs
+mkdir -p logs
+
+export MULTI_STREAM_MEMORY_REUSE=2
+export ASCEND_RT_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export PYTORCH_NPU_ALLOC_CONF=expandable_segments:True
+
 ps aux | grep train | grep -v grep | awk '{print $2}' | xargs kill -9
-torchrun --nproc_per_node=8 --master_port=29502 train_fsdp2.py 2>&1 | tee .logs/$(date +%Y%m%d%H%M%S).log
+torchrun --nproc_per_node=8 train_fsdp2.py 2>&1 | tee logs/$(date +%Y%m%d%H%M%S).log
