572870ce42d0288e8b4eed2dbfb66d46dec33e4c
icerain-alt <450125138@qq.com>
2026-02-06 19:30:22 +0800

    update training utils

---
+++ utils/__init__.py
@@ -0,0 +1,11 @@
+from utils.utils import (
+    format_metrics_to_gb,
+    print_model_info,
+    seed_all,
+    load_fsdp_model,
+    save_fsdp_model,
+    save_fsdp2_model,
+    load_fsdp2_model,
+    is_torch_npu_available,
+    gradient_checkpointing
+)
